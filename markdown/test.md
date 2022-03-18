53d0f959cbaeeba2fc81c1b50f8ef77eeec969d5
mariagalvez <maria.galvez@idneo.com>
2022-03-18 12:56:30 +0100

    first commit

---
+++ markdown/test.md
@@ -0,0 +1,14 @@
+# Heading 1
+
+## Heading 2 text
+  
+Hello world!
+We will output Markdown to:
+
+- HTML
+- docx
+- PDF
+
+----------
+*Text in italic*
+Link to [Google](http://www.google.com/)
---
+++ python/app.py
@@ -0,0 +1,7 @@
+kilometers = float(input("Enter value in kilometers: "))
+
+conversion = 0.621371
+
+miles = kilometers * conversion
+
+print("%0.2f kilometers in miles is equivalent to %0.2f" %(kilometers,miles))
